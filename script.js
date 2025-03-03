7a69dd9cf55d5e7dc92b4975416cab9a123098fe
prabesh340 <prabeshdeskop@gmail.com>
2025-03-03 22:18:19 +0545

    commit_3

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-var tl = gsap.timeline();
+const loader = () => {
+  var tl = gsap.timeline();
 
-tl.to(".yellow",{
-    top:"-100%",
-    ease:"expo.out"
-})
-tl.to("video,.loader",{
-    top:"-100%",
-    delay:0.6,
-})
-tl.from(".page1 h1",{
-    color:"#fff",
-    ease:"expo.out"
-},"=-0.4")
+  tl.to(".yellow", {
+    top: "-100%",
+    ease: "expo.out",
+  });
+  tl.to("video,.loader", {
+    top: "-100%",
+    delay: 0.6,
+  });
+  tl.from(
+    ".text h1",
+    {
+      color: "#fff",
+      ease: "expo.out",
+    },
+    "=-0.4"
+  );
 
-tl.to(".loader",{
-    opacity:0,
-})
+  tl.to(
+    ".loader,.text",
+    {
+      opacity: 0,
+      display: "none",
+    },
+    "=-0.2"
+  );
+};
+loader();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
